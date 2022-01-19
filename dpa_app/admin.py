d46ab66aa199ebdbe185d57272b1be239b394a02
Wolkodlakus <lesko68@list.ru>
2022-01-19 18:33:13 +0300

    Register models to admin.py

--- dpa_app/admin.py
+++ dpa_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import TimeSlot, PM, Group, Student
+
+admin.site.register(PM)
+admin.site.register(Student)
+admin.site.register(TimeSlot)
+admin.site.register(Group)
 
 # Register your models here.
