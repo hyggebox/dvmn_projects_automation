b4bfd1646ac004f7d80aa7b4c820e67da9d402de
Anastasia <asya_apto@mail.ru>
2022-01-20 16:54:02 +0300

    Save data got from the form to DB

--- dpa_app/views.py
+++ dpa_app/views.py
@@ -6,22 +6,22 @@ from .models import Student
 
 
 def show_time_slots(request, user_id):
-    if request.method == 'POST':
-        form = ChooseTimeForm(request.POST)
-        if form.is_valid():
-            # Полученные из таблицы данные
-            print(form.cleaned_data.get('best_time_slots')) # удобные временные слоты
-            print(form.cleaned_data.get('ok_time_slots'))  # возможные слоты
-            print(form.data.getlist('best_time_slots'))  # id удобных слотов
-            print(form.data.getlist('ok_time_slots'))  # id возможных слотов
-            print(form.data.get('user_id'))  # user_id из адреса
-
-            return HttpResponseRedirect('/thanks/')
     try:
         student = Student.objects.get(tg_id=user_id)
     except Student.DoesNotExist:
         raise Http404('Page Not Found')
     else:
+        if request.method == 'POST':
+            form = ChooseTimeForm(request.POST)
+            if form.is_valid():
+                best_time_slots = form.cleaned_data.get('best_time_slots')
+                ok_time_slots = form.cleaned_data.get('ok_time_slots')
+
+                student.best_time_slots.set(best_time_slots)
+                student.ok_time_slots.set(ok_time_slots)
+
+                return HttpResponseRedirect('/thanks/')
+
         form = ChooseTimeForm()
         return render(request, 'choose_time.html',
                       context={
