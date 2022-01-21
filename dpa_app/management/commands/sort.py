15d55cf6fb2c2f132514ffc37463a6f7eb37770e
Anastasia <asya_apto@mail.ru>
2022-01-22 00:39:10 +0300

    Automate the script more

--- dpa_app/management/commands/sort.py
+++ dpa_app/management/commands/sort.py
@@ -1,79 +1,55 @@
-
 import os
 import json
+import networkx as nx
+
 from dpa_app.models import TimeSlot, PM, Group, Student
 from django.core.management.base import BaseCommand
 from datetime import datetime
 from dvmn_projects_automation.settings import BASE_DIR
-import networkx as nx
-# Сортировка
 
-G = nx.DiGraph()
 
-slot1 = "18:00-18:30"
-slot2 = "18:30-19:00"
-slot3 = "19:00-19:30"
-slot4 = "19:30-20:00"
-slot5 = "20:00-20:30"
-slot6 = "20:30-21:00"
+def get_capacities():
+    time_slots = TimeSlot.objects.all()
+    capacities = {slot.id: (slot.pms.all().count() * 3) for slot in time_slots}
+    return capacities
 
-slot_nums = [slot1,slot2,slot3,slot4,slot5,slot6]
-
-capacities = {
-    slot1:3,
-    slot2:3,
-    slot3:6,
-    slot4:6,
-    slot5:3,
-    slot6:3
-}
-
-prefers = {
-    'Student1':[slot3, slot4, slot6],
-    'Student2':[slot3, slot4, slot6],
-    'Student3':[slot5, slot4],
-    'Student4':[slot2],
-    'Student5':[slot1, slot4, slot5, slot6],
-    'Student6':[slot3, slot4, slot6],
-    'Student7':[slot4, slot6],
-    'Student8':[slot3],
-    'Student9':[slot1, slot2, slot3, slot4, slot5, slot6],
-    'Student10':[slot1, slot2, slot3, slot4, slot5, slot6],
-    'Student11':[slot2],
-    'Student12':[slot2, slot3],
-    'Student13':[slot5],
-    'Student14':[slot5, slot6],
-    'Student15':[slot1, slot2, slot3, slot4, slot5, slot6],
-    'Student16':[slot1, slot4, slot6],
-    'Student17':[slot2, slot4, slot6],
-    'Student18':[slot5],
-    'Student19':[slot3],
-    'Student20':[slot2, slot4, slot6],
-    'Student21':[slot2, slot4, slot6],
-       }
 
 def load_student_slots():
     prefs = {}
     students = Student.objects.all()
+
     for student in students:
-        slots = student.best_time_slots.all()
-        list_slots = []
-        for slot in slots:
+        best_slots = student.best_time_slots.all()
+        ok_slots = student.ok_time_slots.all()
+        list_slots = [slot.id for slot in best_slots]
+        for slot in ok_slots:
+            list_slots.append(slot.id)
 
-            list_slots.append(slot.timeslot)
-
-        prefs[student.id]=list_slots
+        prefs[student.id] = list_slots
     return prefs
 
+
+def print_groups(groups_dict):
+    for slot_id, students_ids in groups_dict.items():
+        try:
+            print(f'\nВремя {TimeSlot.objects.get(id=slot_id)}:')
+        except ValueError:
+            pass
+        else:
+            for num, student_id in enumerate(students_ids):
+                student = Student.objects.get(id=student_id)
+                print(num+1, student.f_name, student.l_name)
+
+
 def main() -> None:
-    prefs=load_student_slots()
+    graph = nx.DiGraph()
+    prefs = load_student_slots()
 
     persons_num = len(prefs)
-    G.add_node('dest', demand=persons_num)
-    A = []
+    graph.add_node('dest', demand=persons_num)
 
     for person, time_slots in prefs.items():
-        G.add_node(person, demand=-1)
+        graph.add_node(person, demand=-1)
         for i, time_slot in enumerate(time_slots):
             if i == 0:
                 points = -100  # лучшее время
@@ -87,23 +63,22 @@ def main() -> None:
                 points = -20
             else:
                 points = -1  # худшее время
-            G.add_edge(person, time_slot, capacity=1, weight=points)
+            graph.add_edge(person, time_slot, capacity=1, weight=points)
 
-    for time_slot, capacity in capacities.items():
-            G.add_edge(time_slot, 'dest', capacity=capacity, weight=0)
+    for time_slot, capacity in get_capacities().items():
+            graph.add_edge(time_slot, 'dest', capacity=capacity, weight=0)
 
-    flowdict = nx.min_cost_flow(G)
+    flowdict = nx.min_cost_flow(graph)
     groups = {}
     for person in prefs:
         for time_slot, flow in flowdict[person].items():
             if flow:
                 groups[time_slot] = groups.get(time_slot, []) + [person]
 
-    print(groups)
+    print_groups(groups)
 
 
 class Command(BaseCommand):
-
 
     def handle(self, *args, **options):
         """
