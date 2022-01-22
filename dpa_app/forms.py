c4653667183583db7d5b6d5e702723b24d45601a
Anastasia <asya_apto@mail.ru>
2022-01-22 13:04:26 +0300

    Fix form labels

--- dpa_app/forms.py
+++ dpa_app/forms.py
@@ -11,13 +11,13 @@ class PMForm(forms.ModelForm):
 
 class ChooseTimeForm(forms.Form):
     best_time_slots = forms.ModelMultipleChoiceField(
-        label='Наиболее удобное время',
+        label='Наиболее удобное время (*время московское)',
         queryset=TimeSlot.objects.all(),
         widget=forms.CheckboxSelectMultiple,
         required=False
     )
     ok_time_slots = forms.ModelMultipleChoiceField(
-        label='Менее удобное время, но возможное',
+        label='Менее удобное время, но возможное (*время московское)',
         queryset=TimeSlot.objects.all(),
         widget=forms.CheckboxSelectMultiple,
         required=False
