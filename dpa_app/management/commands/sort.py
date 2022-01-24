2a0f154d010a75256d4508c9031b710981664bd2
Anastasia <asya_apto@mail.ru>
2022-01-24 23:32:54 +0300

    Add actual script

--- dpa_app/management/commands/sort.py
+++ dpa_app/management/commands/sort.py
@@ -1,81 +1,148 @@
-import os
-import json
-import networkx as nx
+from django.core.management.base import BaseCommand
 
 from dpa_app.models import TimeSlot, PM, Group, Student
-from django.core.management.base import BaseCommand
-from datetime import datetime
-from dvmn_projects_automation.settings import BASE_DIR
 
 
-def get_capacities():
-    time_slots = TimeSlot.objects.all()
-    capacities = {slot.id: (slot.pms.all().count() * 3) for slot in time_slots}
-    return capacities
+def get_pms_time_slots():
+    '''Returns a tuple of available time_slots with PMs from DB'''
+    pms = PM.objects.all()
+    available_time_slots = []
+    for pm in pms:
+        time_slots = pm.time_slots.all()
+        for slot in time_slots:
+            available_time_slots.append((slot.id, pm.name))
+    return available_time_slots
 
 
 def load_student_slots():
+    '''Returns a dict of student ids with preferred time slots'''
     prefs = {}
     students = Student.objects.all()
-
     for student in students:
         best_slots = student.best_time_slots.all()
-        ok_slots = student.ok_time_slots.all()
         list_slots = [slot.id for slot in best_slots]
-        for slot in ok_slots:
-            list_slots.append(slot.id)
-
         prefs[student.id] = list_slots
     return prefs
 
 
-def print_groups(groups_dict):
-    for slot_id, students_ids in groups_dict.items():
-        try:
-            print(f'\nВремя {TimeSlot.objects.get(id=slot_id)}:')
-        except ValueError:
-            pass
-        else:
-            for num, student_id in enumerate(students_ids):
+def load_students_secondary_slots(student_id):
+    '''Returns a list of student's possible time slots'''
+    student = Student.objects.get(id=student_id)
+    ok_slots = student.ok_time_slots.all()
+    ok_slots_ids = [slot.id for slot in ok_slots]
+    return ok_slots_ids
+
+
+def print_groups(groups, temp_ids):
+    for slot_temp_id, student_ids in groups.items():
+        slot_id, pm_name = temp_ids[slot_temp_id]
+        slot = TimeSlot.objects.get(id=slot_id)
+        print(slot.timeslot, pm_name)
+        for student_id in student_ids:
+            student = Student.objects.get(id=student_id)
+            print(f'{student.f_name} {student.l_name} -- ({student.level})')
+        print('')
+
+
+def create_groups(groups, temp_ids):
+    for slot_temp_id, student_ids in groups.items():
+        if student_ids:
+            slot_id, pm_name = temp_ids[slot_temp_id]
+            slot = TimeSlot.objects.get(id=slot_id)
+            groups_pm = PM.objects.get(name=pm_name)
+            group, created = Group.objects.get_or_create(
+                pm=groups_pm,
+                time_slot=slot
+            )
+            for student_id in student_ids:
                 student = Student.objects.get(id=student_id)
-                print(num+1, student.f_name, student.l_name)
+                student.group = group
+                student.save()
 
 
-def main() -> None:
-    graph = nx.DiGraph()
-    prefs = load_student_slots()
+def sort_students_for_groups(groups, temporary_ids_for_slots, students):
 
-    persons_num = len(prefs)
-    graph.add_node('dest', demand=persons_num)
+    for temp_id, (slot_id, pm_name) in temporary_ids_for_slots.items():
+        students_in_group = []
+        first_student_id_in_group = 0
+        for student_id, student_answer in students.items():
 
-    for person, time_slots in prefs.items():
-        graph.add_node(person, demand=-1)
-        for i, time_slot in enumerate(time_slots):
-            if i == 0:
-                points = -100  # лучшее время
-            elif i == 1:
-                points = -80
-            elif i == 2:
-                points = -60
-            elif i == 3:
-                points = -40
-            elif i == 4:
-                points = -20
-            else:
-                points = -1  # худшее время
-            graph.add_edge(person, time_slot, capacity=1, weight=points)
+            student = Student.objects.get(id=student_id)
+            students_level = student.level
 
-    for time_slot, capacity in get_capacities().items():
-            graph.add_edge(time_slot, 'dest', capacity=capacity, weight=0)
+            for chosen_slot in student_answer:
 
-    flowdict = nx.min_cost_flow(graph)
+                if chosen_slot == slot_id \
+                    and len(students_in_group) == 0:
+                        students_in_group.append(student_id)
+                        first_student_id_in_group = student_id
+
+                elif chosen_slot == slot_id \
+                    and len(students_in_group) < 3 \
+                    and first_grupped_student_lvl == students_level:
+                        students_in_group.append(student_id)
+                        first_student_id_in_group = groups[last_id][0]
+
+                groups.update({temp_id: students_in_group})
+                last_id = temp_id
+            if first_student_id_in_group != 0:
+                first_grupped_student_lvl = Student.objects.get(
+                    id=first_student_id_in_group
+                    ).level
+
+        for student_id in students_in_group:
+            del students[student_id]
+
+        sorted_groups = groups
+    return sorted_groups, temporary_ids_for_slots, students
+
+
+def sort_students_by_secondary_slots(groups, temporary_ids_for_slots, student_id, pref_slots_ids):
+    for temp_id, (slot_id, pm_name) in temporary_ids_for_slots.items():
+        pass
+
+    for group_temp_slot_id, student_ids in groups.items():
+        if student_ids:
+            first_groupped_student_lvl = Student.objects.get(id=student_ids[0]).level
+            current_student_lvl = Student.objects.get(id=student_id).level
+            if len(student_ids) < 3 and first_groupped_student_lvl == current_student_lvl:
+                slot_id, groups_pm = temporary_ids_for_slots[group_temp_slot_id]
+                if slot_id in pref_slots_ids:
+                    groups[group_temp_slot_id].append(student_id)
+                    return True
+    return False
+
+
+def main():
+    available_time_slots = get_pms_time_slots()
+
+    temporary_ids_for_slots = {}
+    for num, slot in enumerate(available_time_slots):
+        temporary_ids_for_slots[num] = slot
+
+    students = load_student_slots()
     groups = {}
-    for person in prefs:
-        for time_slot, flow in flowdict[person].items():
-            if flow:
-                groups[time_slot] = groups.get(time_slot, []) + [person]
+    sorted_groups, _, not_included_students = \
+        sort_students_for_groups(groups, temporary_ids_for_slots, students)
 
-    print_groups(groups)
+    secondary_not_included_students = []
+    for student_id, _ in not_included_students.items():
+        students_secondary_slots = load_students_secondary_slots(student_id)
+        if not sort_students_by_secondary_slots(groups,
+                                         temporary_ids_for_slots,
+                                         student_id,
+                                         students_secondary_slots):
+            secondary_not_included_students.append(student_id)
+
+    for group_temp_slot_id, student_ids in groups.items():
+        if len(student_ids) < 2:
+            groups[group_temp_slot_id] = []
+            secondary_not_included_students += student_ids
+
+    create_groups(groups, temporary_ids_for_slots)
+    print('Id студентов, не вошедших в группы:')
+    print(secondary_not_included_students)
+
 
 
 class Command(BaseCommand):
